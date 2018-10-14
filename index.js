002f5536b10a1b0cf02d0515a23c6c12fc8fddd2
simpart <simpart>
2018-10-14 15:31:52 +0000

    add comment

--- index.js
+++ index.js
@@ -1,63 +1,80 @@
 /**
  * @file mofron-effect-invclr/index.js
+ * @brief invert color effect 
  * @author simpart
  */
 const mf    = require('mofron');
 const evInv = require('mofron-event-invclr');
-
 /**
  * @class Invclr
  * @brief invert color effect class
  */
 mf.effect.Invclr = class extends mf.Effect {
     
-    constructor (po, p2, p3) {
+    constructor (po, p2) {
         try {
             super();
             this.name('Invclr');
-            this.prmMap('invertEvent', 'colorTgt');
-            this.prmOpt(po, p2, p3);
+            this.prmMap(['targetComp', 'targetColor']);
+            this.prmOpt(po, p2);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    invertEvent (prm, tgt) {
+    /**
+     * set component and target color for invert event
+     * it is target event for kick this effect
+     * 
+     * @param cmp (Component) target invert component
+     * @param tgt (string) target invert color, it kicks this effect when this parameter inverts.
+     * @return 
+     */
+    targetComp (cmp, tgt) {
         try {
-            let ret = this.member('invertEvent', new evInv(), prm);
-            if (undefined !== prm) {
-                if (true !== mf.func.isInclude(tgt, 'Component')) {
-                    throw new Error('invalid parameter');
-                }
-                tgt.execOption({ event : prm })
-                prm.kickEffect(this);
+            if (true !== mf.func.isInclude(cmp, 'Component')) {
+                throw new Error('invalid parameter');
             }
-            return ret;
+            let ev_inv = new evInv({
+                targetColor : tgt,
+                kickEffect  : this
+            });
+            cmp.execOption({ event : ev_inv });
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    colorTgt (prm) {
-        try {
-            return this.member('colorTgt', 'background', prm);
-        } catch (e) {
+    /**
+     * setter/getter for effect target color
+     * this color will chenge when target color was invert
+     * 
+     * @param tgt (string) css color key, default key is 'background'
+     * @return (string) css color key
+     */
+    targetColor (tgt) {
+        try { return this.member('targetColor', 'string', tgt, 'background'); } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * effect contents
+     *
+     * @note private method
+     */
     contents (flg, cmp) {
         try {
             super.contents(flg, cmp);
-            if ( ("mainColor"   === this.colorTgt()) ||
-                 ("baseColor"   === this.colorTgt()) ||
-                 ("accentColor" === this.colorTgt()) ) {
-                cmp[this.colorTgt()](this.enableColor());
+            if ( ("mainColor"   === this.targetColor()) ||
+                 ("baseColor"   === this.targetColor()) ||
+                 ("accentColor" === this.targetColor()) ) {
+                cmp[this.targetColor()](this.enableColor());
             } else {
-                cmp.tgtColor(this.colorTgt(), (true === flg) ? this.enableColor() : this.disableColor());
+                cmp.tgtColor(this.targetColor(), (true === flg) ? this.enableColor() : this.disableColor());
             }
         } catch (e) {
             console.error(e.stack);
@@ -68,12 +85,18 @@ mf.effect.Invclr = class extends mf.Effect {
     enable (cmp) {}
     disable (cmp) {}
     
-    enableColor (prm) {
+    /**
+     * setter/getter for enable color
+     * this color will set when target color of targetComp was invert
+     * 
+     */
+    enableColor (clr) {
         try {
             return this.member(
                 'enableColor',
-                new mf.Color(255,255,255),
-                (undefined === prm) ? prm : mf.func.getColor(prm)
+                'Color',
+                (undefined === clr) ? undefined : mf.func.getColor(clr),
+                new mf.Color(255,255,255)
             );
         } catch (e) {
             console.error(e.stack);
@@ -81,6 +104,12 @@ mf.effect.Invclr = class extends mf.Effect {
         }
     }
     
+    /**
+     * setter/getter for enable color
+     * this color will set when target color of targetComp returned from invert
+     * 
+     * @param
+     */
     disableColor (prm) {
         try {
             return this.member(
